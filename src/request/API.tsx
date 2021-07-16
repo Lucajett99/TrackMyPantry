f0d72f94eaaf06743192d0fe69e37dfe9c9cde61
Luca Genova <48278825+Lucajett99@users.noreply.github.com>
2021-07-16 19:27:20 +0200

    Update API.tsx

--- src/request/API.tsx
+++ src/request/API.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const baseURL = 'https://lam21.modron.network'
 
+//login
 export function login(email: any, password: any) {
         const loginData = {
                 email: email,
@@ -11,6 +12,26 @@ export function login(email: any, password: any) {
                 headers: {'Access-Control-Allow-Origin' : '*', 'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'}
         });
         api.post('/auth/login', loginData)
+        .then((res) => {
+                console.log(res.data);
+        })
+        .catch((error) => {
+                console.log(error);
+        });
+}
+
+//register
+export function register(username: any, email: any, password: any) {
+        const registerData = {
+                username: username,
+                email: email,
+                password: password
+        };
+        const api = axios.create({
+                baseURL: baseURL,
+                headers: {'Access-Control-Allow-Origin' : '*', 'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'}
+        });
+        api.post('/users', registerData)
         .then((res) => {
                 console.log(res.data);
         })
